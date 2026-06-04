c2921a40daf8c1c338139d8aa4d163b652d91fc5
JuanVi75 <juanvipaezosp@gmail.com>
2026-06-04 08:39:13 -0500

    Update calendar.js

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -12,8 +12,6 @@ document.addEventListener("DOMContentLoaded", function () {
         yearEnd
     ) {
 
-        let festivos = [];
-
         // =====================================
         // FORMAT DATE
         // =====================================
